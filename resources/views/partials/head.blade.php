0b772a607ea89dd3ce58b595171cb20f735982a4
Hafiz <arepis123@gmail.com>
2026-01-29 16:38:26 +0800

    Main

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,9 +4,12 @@
 
 <title>e-Salary CLAB</title>
 
-<link rel="icon" href="/logo-clab.png" sizes="any">
-<link rel="icon" href="/logo-clab.png" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="{{ asset('images/favicon-96x96.png') }}" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="{{ asset('images/favicon.svg') }}" />
+<link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" />
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}" />
+<meta name="apple-mobile-web-app-title" content="e-Salary" />
+<link rel="manifest" href="{{ asset('images/site.webmanifest') }}" />
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=inter:400,500,600|fredoka:400|albert-sans:600&display=swap" rel="stylesheet" />
